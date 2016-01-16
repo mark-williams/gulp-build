5bb2eeab17ba674a3d02eb1908f358c5d99defeb
mark-williams <markwill@hotmail.co.uk>
2016-01-16 15:14:03 +0000

    =Added gulp task to create an initial build, combining multiple assets into one file if indicated by 'build' comments in html files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,10 @@ var gulp = require('gulp');
 var config = require('./gulp.config')();
 var wiredep = require('wiredep').stream;
 var inject = require('gulp-inject');
+var useref = require('gulp-useref');
  
+var assets = useref({searchPath: './'});
+
 gulp.task('default', function() {
    console.log('Gulp!!!'); 
 });
@@ -17,4 +20,13 @@ gulp.task('inject-refs', function() {
         .pipe(inject(gulp.src(config.css)))
         .pipe(gulp.dest(config.client))
         ;    
+});
+
+gulp.task('optimise', ['inject-refs'], function() {
+    return gulp
+        .src(config.index)
+        .pipe(assets)
+        //.pipe(assets.restore())
+        .pipe(gulp.dest(config.build))
+        ;
 });
